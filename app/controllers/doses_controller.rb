3418554ff22d073e94824df30a86a453ec508cd6
Geert Keularts <geertkeularts@gmail.com>
2016-06-02 17:51:54 +0200

    improve remove dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -18,6 +18,7 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.find(params[:id])
     @dose.destroy
     redirect_to cocktail_path(@cocktail)

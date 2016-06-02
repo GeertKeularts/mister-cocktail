fd6c90b7187df177665eec38f8f80a3d7ccfb072
Geert Keularts <geertkeularts@gmail.com>
2016-06-02 12:33:01 +0200

    add models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,4 +2,7 @@ class Dose < ActiveRecord::Base
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
